231d0cb59c9b0c93f6a4ff6dcd69655d807f90d6
akkv <a-k-k-v@yandex.ru>
2018-06-05 18:52:14 +0300

    Статистика2

--- js/stats.js
+++ js/stats.js
@@ -1,3 +1,4 @@
+'use strict';
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 240;
 var CLOUD_X = 100;
@@ -42,7 +43,7 @@ window.renderStatistics = function (ctx, names, times) {
     times[i] = Math.round(times[i]);
     var randForBlue = Math.floor(2 + Math.random() * 9);
     var blue = 'rgba(0, 0, 128, 0.' + randForBlue + ')';
-    names[i] === 'Вы' ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = blue;
+    ctx.fillStyle = (names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : blue);
     ctx.fillRect(CLOUD_X + (GAP * (i + 1) + BAR_WIDTH * i), CLOUD_Y + CLOUD_HEIGHT - BAR_HEIGHT - VERTICAL_GAP + (BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime), BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
     ctx.fillStyle = '#000';
     ctx.fillText(times[i], CLOUD_X + (GAP * (i + 1) + BAR_WIDTH * i), CLOUD_Y + CLOUD_HEIGHT - BAR_HEIGHT - VERTICAL_GAP + (BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime) - 3);
