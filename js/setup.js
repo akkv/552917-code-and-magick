09041e5e539e60965725b90c373600686b415d39
akkv <a-k-k-v@yandex.ru>
2018-06-21 21:03:51 +0300

    одетьНадежду

---
+++ js/setup.js
@@ -0,0 +1,110 @@
+'use strict';
+var getRandomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var exampleNamesParts = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var exampleSurnamesParts = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var exampleCoatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var exampleEyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('div');
+var wizardsContainer = document.querySelector('.setup-similar-list');
+
+var generateWizardsData = function (amount) {
+  var wizards = [];
+  for (var i = 0; i < amount; i++) {
+    wizards.push({
+      name: exampleNamesParts[getRandomInt(0, exampleNamesParts.length)] + ' ' + exampleSurnamesParts[getRandomInt(0, exampleSurnamesParts.length)],
+      coatColor: exampleCoatColors[getRandomInt(0, exampleCoatColors.length)],
+      eyesColor: exampleEyesColors[getRandomInt(0, exampleEyesColors.length)]
+    });
+  }
+  return wizards;
+};
+
+var createWizardElement = function (wizardData) {
+  var wizard = wizardTemplate.cloneNode(true);
+  wizard.querySelector('.setup-similar-label').textContent = wizardData.name;
+  wizard.querySelector('.wizard-coat').style.fill = wizardData.coatColor;
+  wizard.querySelector('.wizard-eyes').style.fill = wizardData.eyesColor;
+  return wizard;
+};
+
+var createWizardsSimilarList = function (wizardsData) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizardsData.length; i++) {
+    fragment.appendChild(createWizardElement(wizardsData[i]));
+  }
+  return fragment;
+};
+
+wizardsContainer.appendChild(createWizardsSimilarList(generateWizardsData(4)));
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
+var setupClose = document.querySelector('.setup-close');
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+var userNameInput = setup.querySelector('.setup-user-name');
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Введите не менее 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Введите неболее 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+var exampleFireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+var coat = document.querySelector('.setup-wizard .wizard-coat');
+var eyes = document.querySelector('.setup-wizard .wizard-eyes');
+var fireball = document.querySelector('.setup-fireball-wrap');
+
+coat.addEventListener('click', function () {
+  coat.style.fill = exampleCoatColors[getRandomInt(0, exampleCoatColors.length)];
+});
+eyes.addEventListener('click', function () {
+  eyes.style.fill = exampleEyesColors[getRandomInt(0, exampleEyesColors.length)];
+});
+fireball.addEventListener('click', function () {
+  fireball.style.backgroundColor = exampleFireballColors[getRandomInt(0, exampleFireballColors.length)];
+  fireball.querySelector('input').value = fireball.style.backgroundColor;
+});
